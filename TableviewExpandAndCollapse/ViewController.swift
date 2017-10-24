937d227d846d7581c1e0f6b757532a4adce04ee0
Ammy <ammypandey@Ammys-MacBook-Air.local>
2017-10-24 18:12:41 +0530

    Initial Commit

---
+++ TableviewExpandAndCollapse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableviewExpandAndCollapse
+//
+//  Created by Ammy Pandey on 24/10/17.
+//  Copyright © 2017 Ammy Pandey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
